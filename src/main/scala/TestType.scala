7ffcd17a0cff4997cd14c946d4c17fe1427e25ae
Donggyu Kim <dgkim@eecs.berkeley.edu>
2016-10-18 15:21:43 -0700

    put TestType in src/main/scala

---
+++ src/main/scala/TestType.scala
@@ -0,0 +1,29 @@
+package mini
+
+trait TestType {
+  def tests: List[String]
+  def maxcycles: Long
+}
+
+case object SimpleTests extends TestType {
+  val tests = List("rv32ui-p-simple")
+  val maxcycles = 15000L
+}
+
+case object ISATests extends TestType {
+  val tests = (List("simple", "add", "addi", "auipc", "and", "andi", // TODO: "fence_i",
+    "sb", "sh", "sw", "lb", "lbu", "lh", "lhu", "lui", "lw",
+    "beq", "bge", "bgeu", "blt", "bltu", "bne", "j", "jal", "jalr",
+    "or", "ori", "sll", "slli", "slt", "slti", "sra", "srai", "sub", "xor", "xori"
+  ) map (t => s"rv32ui-p-${t}")) ++ (List(
+    "sbreak", "scall", "illegal", "ma_fetch", "ma_addr", "csr" //, TODO: "timer"
+  ) map (t => s"rv32mi-p-${t}"))
+  val maxcycles = 15000L
+}
+
+case object BmarkTests extends TestType {
+  val tests = List(
+    "median.riscv", "multiply.riscv", "qsort.riscv", "towers.riscv", "vvadd.riscv"
+  )
+  val maxcycles = 1500000L
+}
--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -54,31 +54,6 @@ class DatapathUnitTest extends UnitTest(new Datapath()(p))(m => new DatapathTest
 class CacheUnitTest extends UnitTest(new Cache()(p))(m => new CacheTests(m))
 class CoreUnitTest extends UnitTest(new Core()(p))(m => new CoreSimpleTests(m))
 
-trait TestType {
-  def tests: List[String]
-  def maxcycles: Long
-}
-case object SimpleTests extends TestType {
-  val tests = List("rv32ui-p-simple")
-  val maxcycles = 15000L
-}
-case object ISATests extends TestType {
-  val tests = (List("simple", "add", "addi", "auipc", "and", "andi", // TODO: "fence_i",
-    "sb", "sh", "sw", "lb", "lbu", "lh", "lhu", "lui", "lw",
-    "beq", "bge", "bgeu", "blt", "bltu", "bne", "j", "jal", "jalr",
-    "or", "ori", "sll", "slli", "slt", "slti", "sra", "srai", "sub", "xor", "xori"
-  ) map (t => s"rv32ui-p-${t}")) ++ (List(
-    "sbreak", "scall", "illegal", "ma_fetch", "ma_addr", "csr" //, TODO: "timer"
-  ) map (t => s"rv32mi-p-${t}"))
-  val maxcycles = 15000L
-}
-case object BmarkTests extends TestType {
-  val tests = List(
-    "median.riscv", "multiply.riscv", "qsort.riscv", "towers.riscv", "vvadd.riscv"
-  )
-  val maxcycles = 1500000L
-}
-
 abstract class MiniTestSuite[+T <: Module : ClassTag](
     dutGen: => T,
     backend: String,
