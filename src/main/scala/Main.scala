6b889ec7ba5d72876f764532220725d49c49b912
Donggyu Kim <dgkim@eecs.berkeley.edu>
2016-10-18 23:12:08 -0700

    dump chirrtl

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,11 +3,15 @@ package mini
 import java.io.{File, FileWriter}
 
 object Main extends App {
-  // Compile for synthesis
   val dir = new File(args(0)) ; dir.mkdirs
   val params = cde.Parameters.root((new MiniConfig).toInstance)
   val chirrtl = firrtl.Parser.parse(chisel3.Driver.emit(() => new Tile(params)))
+  val writer = new FileWriter(new File(dir, s"${chirrtl.main}.fir"))
+  firrtl.FIRRTLEmitter run (chirrtl, writer)
+  writer.close
+
   val annotations = new firrtl.Annotations.AnnotationMap(Nil)
   val verilog = new FileWriter(new File(dir, s"${chirrtl.main}.v"))
   new firrtl.VerilogCompiler compile (chirrtl, annotations, verilog)
+  verilog.close
 }
