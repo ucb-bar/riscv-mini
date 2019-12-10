1816b3886b32df213c824dc128c4882f6c506b0c
chick <chick@qrhino.com>
2019-12-10 12:11:35 -0800

    Turn Trace to false in config Too much output for CI

--- src/main/scala/mini/Config.scala
+++ src/main/scala/mini/Config.scala
@@ -9,7 +9,7 @@ import junctions._
 class MiniConfig extends Config((site, here, up) => {
     // Core
     case XLEN => 32
-    case Trace => true
+    case Trace => false
     case BuildALU    => (p: Parameters) => Module(new ALUArea()(p))
     case BuildImmGen => (p: Parameters) => Module(new ImmGenWire()(p))
     case BuildBrCond => (p: Parameters) => Module(new BrCondArea()(p))
