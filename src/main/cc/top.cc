9d0194ff3a96ac436d023a55dbe4dfa82e1de11d
chick <chick@qrhino.com>
2019-12-10 13:49:42 -0800

    gets rid of bad fflush in top.cc Main puts firrtl and verilog in generated-src for use with make style tests

--- src/main/cc/top.cc
+++ src/main/cc/top.cc
@@ -93,7 +93,6 @@ int main(int argc, char** argv) {
 #endif
 
   cout << "Starting simulation!\n";
-  fflush(cout);
 
   // reset
   top->reset = 1;
--- src/main/scala/mini/Main.scala
+++ src/main/scala/mini/Main.scala
@@ -2,15 +2,17 @@
 
 package mini
 
-import java.io.{File, FileWriter}
-
 import chisel3.stage.ChiselGeneratorAnnotation
 import firrtl.options.TargetDirAnnotation
 
 object Main extends App {
   val params = (new MiniConfig).toInstance
+
+  val targetDirectory = args.head
+
   new chisel3.stage.ChiselStage().execute(args, Seq(
     ChiselGeneratorAnnotation(() => new Tile(params)),
-    TargetDirAnnotation("test_run_dir")
+    TargetDirAnnotation(targetDirectory)
   ))
 }
+
