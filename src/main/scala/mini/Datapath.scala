d3bfaf99bbd778028e92b402cbf55035f07230ff
Rohan Nawathe <rohannawathe@hotmail.com>
2022-06-27 21:36:36 +0530

    Chisel improvements (#51)
    * Change cascaded Mux to MuxCase
    
    * Rename pipeline registers to different format
    
    * rename npc
    
    * add multithread to verilator and test-quick forsbt
    
    * use IndexedSeq instead of Array for MuxCase
    
    * convert pipeline registers to bundle
    
    * use bundle literals for pipeline register initialisation
    
    Co-authored-by: Rohan Nawathe <rnawathe+riscv@outlook.com>

--- src/main/scala/mini/Datapath.scala
+++ src/main/scala/mini/Datapath.scala
@@ -4,6 +4,7 @@ package mini
 
 import chisel3._
 import chisel3.util._
+import chisel3.experimental.BundleLiterals._
 
 object Const {
   val PC_START = 0x200
@@ -17,6 +18,18 @@ class DatapathIO(xlen: Int) extends Bundle {
   val ctrl = Flipped(new ControlSignals)
 }
 
+class FetchExecutePipelineRegister(xlen: Int) extends Bundle {
+  val inst = chiselTypeOf(Instructions.NOP)
+  val pc = UInt(xlen.W)
+}
+
+class ExecuteWritebackPipelineRegister(xlen: Int) extends Bundle {
+  val inst = chiselTypeOf(Instructions.NOP)
+  val pc = UInt(xlen.W)
+  val alu = UInt(xlen.W)
+  val csr_in = UInt(xlen.W)
+}
+
 class Datapath(val conf: CoreConfig) extends Module {
   val io = IO(new DatapathIO(conf.xlen))
   val csr = Module(new CSR(conf.xlen))
@@ -27,17 +40,27 @@ class Datapath(val conf: CoreConfig) extends Module {
 
   import Control._
 
+  /** Pipeline State Registers * */
+
   /** *** Fetch / Execute Registers ****
     */
-  val fe_inst = RegInit(Instructions.NOP)
-  val fe_pc = Reg(UInt())
+  val fe_reg = RegInit(
+    (new FetchExecutePipelineRegister(conf.xlen)).Lit(
+      _.inst -> Instructions.NOP,
+      _.pc -> 0.U
+    )
+  )
 
   /** *** Execute / Write Back Registers ****
     */
-  val ew_inst = RegInit(Instructions.NOP)
-  val ew_pc = Reg(UInt())
-  val ew_alu = Reg(UInt())
-  val csr_in = Reg(UInt())
+  val ew_reg = RegInit(
+    (new ExecuteWritebackPipelineRegister(conf.xlen)).Lit(
+      _.inst -> Instructions.NOP,
+      _.pc -> 0.U,
+      _.alu -> 0.U,
+      _.csr_in -> 0.U
+    )
+  )
 
   /** **** Control signals ****
     */
@@ -54,27 +77,21 @@ class Datapath(val conf: CoreConfig) extends Module {
   val started = RegNext(reset.asBool)
   val stall = !io.icache.resp.valid || !io.dcache.resp.valid
   val pc = RegInit(Const.PC_START.U(conf.xlen.W) - 4.U(conf.xlen.W))
-  val npc = Mux(
-    stall,
-    pc,
-    Mux(
-      csr.io.expt,
-      csr.io.evec,
-      Mux(
-        io.ctrl.pc_sel === PC_EPC,
-        csr.io.epc,
-        Mux(
-          io.ctrl.pc_sel === PC_ALU || brCond.io.taken,
-          alu.io.sum >> 1.U << 1.U,
-          Mux(io.ctrl.pc_sel === PC_0, pc, pc + 4.U)
-        )
-      )
+  // Next Program Counter
+  val next_pc = MuxCase(
+    pc + 4.U,
+    IndexedSeq(
+      stall -> pc,
+      csr.io.expt -> csr.io.evec,
+      (io.ctrl.pc_sel === PC_EPC) -> csr.io.epc,
+      ((io.ctrl.pc_sel === PC_ALU) || (brCond.io.taken)) -> (alu.io.sum >> 1.U << 1.U),
+      (io.ctrl.pc_sel === PC_0) -> pc
     )
   )
   val inst =
     Mux(started || io.ctrl.inst_kill || brCond.io.taken || csr.io.expt, Instructions.NOP, io.icache.resp.bits.data)
-  pc := npc
-  io.icache.req.bits.addr := npc
+  pc := next_pc
+  io.icache.req.bits.addr := next_pc
   io.icache.req.bits.data := 0.U
   io.icache.req.bits.mask := 0.U
   io.icache.req.valid := !stall
@@ -82,35 +99,34 @@ class Datapath(val conf: CoreConfig) extends Module {
 
   // Pipelining
   when(!stall) {
-    fe_pc := pc
-    fe_inst := inst
+    fe_reg.pc := pc
+    fe_reg.inst := inst
   }
 
   /** **** Execute ****
     */
-  // Decode
-  io.ctrl.inst := fe_inst
+  io.ctrl.inst := fe_reg.inst
 
   // regFile read
-  val rd_addr = fe_inst(11, 7)
-  val rs1_addr = fe_inst(19, 15)
-  val rs2_addr = fe_inst(24, 20)
+  val rd_addr = fe_reg.inst(11, 7)
+  val rs1_addr = fe_reg.inst(19, 15)
+  val rs2_addr = fe_reg.inst(24, 20)
   regFile.io.raddr1 := rs1_addr
   regFile.io.raddr2 := rs2_addr
 
   // gen immdeates
-  immGen.io.inst := fe_inst
+  immGen.io.inst := fe_reg.inst
   immGen.io.sel := io.ctrl.imm_sel
 
   // bypass
-  val wb_rd_addr = ew_inst(11, 7)
+  val wb_rd_addr = ew_reg.inst(11, 7)
   val rs1hazard = wb_en && rs1_addr.orR && (rs1_addr === wb_rd_addr)
   val rs2hazard = wb_en && rs2_addr.orR && (rs2_addr === wb_rd_addr)
-  val rs1 = Mux(wb_sel === WB_ALU && rs1hazard, ew_alu, regFile.io.rdata1)
-  val rs2 = Mux(wb_sel === WB_ALU && rs2hazard, ew_alu, regFile.io.rdata2)
+  val rs1 = Mux(wb_sel === WB_ALU && rs1hazard, ew_reg.alu, regFile.io.rdata1)
+  val rs2 = Mux(wb_sel === WB_ALU && rs2hazard, ew_reg.alu, regFile.io.rdata2)
 
   // ALU operations
-  alu.io.A := Mux(io.ctrl.A_sel === A_RS1, rs1, fe_pc)
+  alu.io.A := Mux(io.ctrl.A_sel === A_RS1, rs1, fe_reg.pc)
   alu.io.B := Mux(io.ctrl.B_sel === B_RS2, rs2, immGen.io.out)
   alu.io.alu_op := io.ctrl.alu_op
 
@@ -120,7 +136,7 @@ class Datapath(val conf: CoreConfig) extends Module {
   brCond.io.br_type := io.ctrl.br_type
 
   // D$ access
-  val daddr = Mux(stall, ew_alu, alu.io.sum) >> 2.U << 2.U
+  val daddr = Mux(stall, ew_reg.alu, alu.io.sum) >> 2.U << 2.U
   val woffset = (alu.io.sum(1) << 4.U).asUInt | (alu.io.sum(0) << 3.U).asUInt
   io.dcache.req.valid := !stall && (io.ctrl.st_type.orR || io.ctrl.ld_type.orR)
   io.dcache.req.bits.addr := daddr
@@ -140,10 +156,10 @@ class Datapath(val conf: CoreConfig) extends Module {
     illegal := false.B
     pc_check := false.B
   }.elsewhen(!stall && !csr.io.expt) {
-    ew_pc := fe_pc
-    ew_inst := fe_inst
-    ew_alu := alu.io.out
-    csr_in := Mux(io.ctrl.imm_sel === IMM_Z, immGen.io.out, rs1)
+    ew_reg.pc := fe_reg.pc
+    ew_reg.inst := fe_reg.inst
+    ew_reg.alu := alu.io.out
+    ew_reg.csr_in := Mux(io.ctrl.imm_sel === IMM_Z, immGen.io.out, rs1)
     st_type := io.ctrl.st_type
     ld_type := io.ctrl.ld_type
     wb_sel := io.ctrl.wb_sel
@@ -154,7 +170,7 @@ class Datapath(val conf: CoreConfig) extends Module {
   }
 
   // Load
-  val loffset = (ew_alu(1) << 4.U).asUInt | (ew_alu(0) << 3.U).asUInt
+  val loffset = (ew_reg.alu(1) << 4.U).asUInt | (ew_reg.alu(0) << 3.U).asUInt
   val lshift = io.dcache.resp.bits.data >> loffset
   val load = MuxLookup(
     ld_type,
@@ -169,11 +185,11 @@ class Datapath(val conf: CoreConfig) extends Module {
 
   // CSR access
   csr.io.stall := stall
-  csr.io.in := csr_in
+  csr.io.in := ew_reg.csr_in
   csr.io.cmd := csr_cmd
-  csr.io.inst := ew_inst
-  csr.io.pc := ew_pc
-  csr.io.addr := ew_alu
+  csr.io.inst := ew_reg.inst
+  csr.io.pc := ew_reg.pc
+  csr.io.addr := ew_reg.alu
   csr.io.illegal := illegal
   csr.io.pc_check := pc_check
   csr.io.ld_type := ld_type
@@ -182,7 +198,11 @@ class Datapath(val conf: CoreConfig) extends Module {
 
   // Regfile Write
   val regWrite =
-    MuxLookup(wb_sel, ew_alu.zext, Seq(WB_MEM -> load, WB_PC4 -> (ew_pc + 4.U).zext, WB_CSR -> csr.io.out.zext)).asUInt
+    MuxLookup(
+      wb_sel,
+      ew_reg.alu.zext,
+      Seq(WB_MEM -> load, WB_PC4 -> (ew_reg.pc + 4.U).zext, WB_CSR -> csr.io.out.zext)
+    ).asUInt
 
   regFile.io.wen := wb_en && !stall && !csr.io.expt
   regFile.io.waddr := wb_rd_addr
@@ -195,8 +215,8 @@ class Datapath(val conf: CoreConfig) extends Module {
 //  if (p(Trace)) {
 //    printf(
 //      "PC: %x, INST: %x, REG[%d] <- %x\n",
-//      ew_pc,
-//      ew_inst,
+//      ew_reg.pc,
+//      ew_reg.inst,
 //      Mux(regFile.io.wen, wb_rd_addr, 0.U),
 //      Mux(regFile.io.wen, regFile.io.wdata, 0.U)
 //    )
